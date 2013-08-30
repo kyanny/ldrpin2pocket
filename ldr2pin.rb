cb0d8b434a9cbd241d1560b206bd6e2619d5b006
Kensuke Nagae <kyanny@gmail.com>
2013-08-31 04:01:27 +0900

    refactor

--- ldr2pin.rb
+++ ldr2pin.rb
@@ -1,67 +1,62 @@
 #!/usr/bin/ruby
-#
-# ldr2pin.rb - Livedoor ReaderのPinをまとめてpinboard.inにブックマークするスクリプト
-#
 
-$KCODE='utf8'
-
-require 'rubygems'
-require 'net/netrc'
 require 'mechanize'
 require 'json'
 require 'cgi'
 require 'open-uri'
-require 'kconv'
 
-netrc_ldr = Net::Netrc.locate "ldr"
-netrc_pin = Net::Netrc.locate "pinboard.in"
-
-# login LDR
-agent = Mechanize.new
-page = agent.get('http://reader.livedoor.com/reader/')
-form = page.form_with(:name => 'loginForm')
-form.livedoor_id = netrc_ldr.login
-form.password = netrc_ldr.password
-agent.submit(form)
-
-# get all pin
-page = agent.post('http://reader.livedoor.com/api/pin/all')
-pin = JSON::parse(page.body)
-
-# post bookmarks to pinboard.in
-#   http://pinboard.in/howto/#api (like delicious.com API)
-post_url = 'https://api.pinboard.in/v1/posts/add'
-post_error = false
-pin.each{|p|
-  link  = p['link']
-  title = p['title'].toutf8
-  puts "title=#{title}"
-  puts "url  =#{link}"
-
-  url = post_url + "?url=#{CGI.escape(link)}&description=#{CGI.escape(title)}"
-
-  begin
-    open(url, :http_basic_authentication=>[netrc_pin.login, netrc_pin.password]){|f|
-      res = f.read
-      unless res =~ /done/
-        puts "==== POST ERROR ===="
-        puts res
-
-        post_error = true
-        break
-      end
-    }
-  rescue Exception => e
-    puts "==== POST ERROR ===="
-    pp e
-    pp e.backtrace
-    post_error = true
-    break
+class LDR
+  def initialize(livedoor_id, password)
+    @livedoor_id = livedoor_id
+    @password = password
   end
-}
 
-# clear all pin
-unless post_error
-  api_key = agent.cookie_jar.jar['reader.livedoor.com']['/']['reader_sid'].value
-  agent.post('http://reader.livedoor.com/api/pin/clear', 'ApiKey' => api_key)
+  def login
+    @agent ||= Mechanize.new
+    page = @agent.get('http://reader.livedoor.com/reader/')
+    form = page.form_with(name: 'loginForm')
+    form.livedoor_id = @livedoor_id
+    form.password = @password
+    @agent.submit(form)
+  end
+
+  def get_all_pins
+    page = @agent.post('http://reader.livedoor.com/api/pin/all')
+    @pins = JSON.parse(page.body)
+  end
+
+  def clear_all_pins
+    unless post_error
+      api_key = @agent.cookie_jar.jar['reader.livedoor.com']['/']['reader_sid'].value
+      @agent.post('http://reader.livedoor.com/api/pin/clear', 'ApiKey' => api_key)
+    end
+  end
+
+  def each_pin(&block)
+    @pins.each do |pin|
+      yield(pin) if block_given?
+    end
+  end
+end
+
+class Pocket
+  def add(url)
+    p url
+  end
+end
+
+@pocket = Pocket.new
+
+@ldr = LDR.new(ENV['LIVEDOOR_ID'], ENV['LIVEDOOR_PASSWORD'])
+@ldr.login
+@ldr.get_all_pins
+@ldr.each_pin do |pin|
+  p pin
+  p['link']
+  p['title']
+  p['title'].encoding
+  CGI.escape(p['link'])
+  CGI.escape(p['title'])
+
+  @pocket.add(p['link'])
 end
